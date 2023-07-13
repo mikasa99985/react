b6b07212691acc05f17764dfe7afd6a8799ed9e9
Soubhik <soubhik960@gmail.com>
2023-07-13 13:13:59 +0530

    Add test db code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,12 @@ import React, { useEffect } from 'react'
 import { database, db } from '../../firebase.config';
 
 const Home = () => {
-  useEffect(() => {
-
-    database.ref('data/test').on("value", function (snapshot) {
-      console.log(snapshot.val());
-    });
-
-    db.collection("test collection").doc("QJhUZNdQqoAu5I0Hahsw").onSnapshot((doc) => {
-      var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-      console.log(source, " data: ", doc.data());
-    });
-
-  }, []);
 
   return (
     <>
       <div className="container mt-4">
         <h1>Test text</h1>
-        <button className='btn btn-danger'>Test button</button>
+        <button className='btn btn-success'>Add data</button>
       </div>
     </>
   )
