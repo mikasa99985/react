b33372d2adc29e9c68958585d3e3e42d7f4801e8
Soubhik <soubhik960@gmail.com>
2023-07-16 22:31:58 +0530

    Add slider button

--- src/components/slidercard.jsx
+++ src/components/slidercard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from './card'
+import icon from '../assets/right-arrow.png'
 
 export default function Slidercard() {
   return (
@@ -53,13 +54,15 @@ export default function Slidercard() {
 
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
             data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            {/* <span className="carousel-control-prev-icon" aria-hidden="true"></span> */}
+            <img src={icon} width={40} style={{rotate: '180deg'}} alt="" />
             <span className="visually-hidden">Previous</span>
           </button>
 
           <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
             data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            {/* <span className="carousel-control-next-icon" aria-hidden="true"></span> */}
+            <img src={icon} width={40} style={{rotate: '0deg'}} alt="" />
             <span className="visually-hidden">Next</span>
           </button>
         </div>
