064715136492365ff84eb56bd808ab17b6d3af9c
Akash <debnathakash911@gmail.com>
2023-07-18 13:23:33 +0530

    i have fixed big card

--- src/components/Bigcard.jsx
+++ src/components/Bigcard.jsx
@@ -10,51 +10,28 @@ export default function Bigcard() {
         <div>
             <div class="d-flex flex-row">
                 <div className="card-deck">
-                    <div className="card"style={{ width: "15rem" }}>
-                        <h4>Asphault</h4>
+                    <div className="card"style={{ width: "20rem" }}>
+                        <h5>Asphault</h5>
                         <p>Racing Game</p>
                         <img src={Anthem } alt="..." />
-                        <div className="card-body">
-                            <a href="">PRICE:$500</a>
-                        </div>
                     </div>
                 </div>
 
                 <div class="d-flex flex-row">
-                    <div className="card" style={{ width: "15rem" }} >
-                         <h4>Anthem</h4>
+                    <div className="card" style={{ width: "20rem" }} >
+                         <h5>Anthem</h5>
                         <p>Online Multiplayer</p>
                         <img src={Anthem} alt="..." />
-                        <div class="card-body">
-
-
-                            <a href="">PRICE:$450</a>
-                        </div>
                     </div>
 
                     <div class="d-flex flex-row">
-                        <div className="card"style={{ width: "15rem" }}>
-                        <h4>HITMAN III</h4>
+                        <div className="card"style={{ width: "20rem" }}>
+                        <h5>HITMAN III</h5>
                         <p>Stealth Game</p>
                             <img src={Anthem} alt="..." />
-                            <div className="card-body">
-
-                                <a href="">PRICE:$300</a>
-                            </div>
                         </div>
 
-                        <div class="d-flex flex-row">
-                            <div className="card"style={{ width: "15rem" }}>
-                            <h4>BattleField</h4>
-                        <p> First person shooter game </p>
-                                <img src={Anthem} alt="..." />
-                                <div className="card-body">
-
-                                    <a href="" >PRICE:$390</a>
-                                </div>
-                            </div>
-                        </div>
-
+                       
                     </div>
                 </div>
             </div>
