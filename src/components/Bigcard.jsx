8c3194f951b26b8dd6829943af1b72d2fe0543a0
Soubhik <soubhik960@gmail.com>
2023-07-18 13:32:28 +0530

    Fix merge error

--- src/components/Bigcard.jsx
+++ src/components/Bigcard.jsx
@@ -10,7 +10,7 @@ export default function Bigcard() {
         <div>
             <div class="d-flex flex-row">
                 <div className="card-deck">
-                    <div className="card"style={{ width: "20rem" }}>
+                    <div className="big-card"style={{ width: "20rem" }}>
                         <h5>Asphault</h5>
                         <p>Racing Game</p>
                         <img src={Anthem } alt="..." />
@@ -18,14 +18,14 @@ export default function Bigcard() {
                 </div>
 
                 <div class="d-flex flex-row">
-                    <div className="card" style={{ width: "20rem" }} >
+                    <div className="big-card" style={{ width: "20rem" }} >
                          <h5>Anthem</h5>
                         <p>Online Multiplayer</p>
                         <img src={Anthem} alt="..." />
                     </div>
 
                     <div class="d-flex flex-row">
-                        <div className="card"style={{ width: "20rem" }}>
+                        <div className="big-card"style={{ width: "20rem" }}>
                         <h5>HITMAN III</h5>
                         <p>Stealth Game</p>
                             <img src={Anthem} alt="..." />
