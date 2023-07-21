5ccb833b7928a6dcd54a5935a5cff627f0583c63
Akash <debnathakash911@gmail.com>
2023-07-21 23:22:00 +0530

    add changes in bigcard

--- src/components/Bigcard.jsx
+++ src/components/Bigcard.jsx
@@ -7,33 +7,62 @@ export default function Bigcard() {
     return (
 
         <div>
-            <div class="d-flex flex-row">
-                <div className="card-deck">
-                    <div className="big-card"style={{ width: "20rem" }}>
-                        <h5>Asphault</h5>
-                        <p>Racing Game</p>
-                        <img src={Anthem } alt="..." />
-                    </div>
+
+
+            <div className="d-flex flex-row">
+                <div className="big-card" style={{ width: "20rem" }} >
+                    <h5>Asphault</h5>
+                    <p>Racing game</p>
+                    <img src={Anthem} alt="..." />
                 </div>
 
-                <div class="d-flex flex-row">
-                    <div className="big-card" style={{ width: "20rem" }} >
-                         <h5>Anthem</h5>
+
+                <div className="d-flex flex-row">
+                    <div className="big-card" style={{ width: "20rem" }}>
+                        <h5>Anthem</h5>
                         <p>Online Multiplayer</p>
                         <img src={Anthem} alt="..." />
                     </div>
 
-                    <div class="d-flex flex-row">
-                        <div className="big-card"style={{ width: "20rem" }}>
-                        <h5>HITMAN III</h5>
-                        <p>Stealth Game</p>
+
+                    <div className="d-flex flex-row">
+                        <div className="big-card" style={{ width: "20rem" }}>
+                            <h5>HITMAN III</h5>
+                            <p>Stealth Game</p>
                             <img src={Anthem} alt="..." />
                         </div>
-
-                       
                     </div>
+
                 </div>
+
+
             </div>
+            <div className="features">
+                    <h4>Features</h4>
+                    <div className="d-flex flex-row">
+                        <div className="feature-card" style={{ width: "20rem" }}>
+                            <h5>HITMAN III</h5>
+                            <p>Stealth Game</p>
+                            <img src=".." alt="..." />
+                        </div>
+                        <div className="d-flex flex-row">
+                        <div className="feature-card" style={{ width: "20rem" }}>
+                            <h5>HITMAN III</h5>
+                            <p>Stealth Game</p>
+                            <img src=".." alt="..." />
+                        </div>
+                        <div className="d-flex flex-row">
+                        <div className="feature-card" style={{ width: "20rem" }}>
+                            <h5>HITMAN III</h5>
+                            <p>Stealth Game</p>
+                            <img src=".." alt="..." />
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                    
+                </div>
+            
         </div>
     )
 
