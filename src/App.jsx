ec622504402e5acd005101c3b85853deb08a47bf
Debjit <debjitroy515@gmail.com>
2023-07-22 03:41:14 +0530

    color

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import {BrowserRouter as Router,Switch,Route,} from "react-router-dom";
-import Home from './pages/Home'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
-
 function App() {
+  const appStyle = {
+    background: 'rgba(255, 187, 0, 0.999)', // Yellow with 50% transparency
+    minHeight: '100vh', // Ensure the background color covers the whole viewport height
+  };
+
   return (
-    <>
+    <div style={appStyle}>
       <Router>
         <Switch>
-       
           <Route exact path="/">
             <Home />
           </Route>
@@ -22,11 +25,10 @@ function App() {
           <Route exact path="/sign_in">
             <SignIn />
           </Route>
-
         </Switch>
       </Router>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
