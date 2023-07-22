8d973f39a79348c02dc0f22ff878107481eb36a9
Akash <debnathakash911@gmail.com>
2023-07-22 13:31:54 +0530

    edit changes in feature

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,20 +8,21 @@ const Features = () => {
                 <h4>Features</h4>
                 <div className="d-flex flex-row">
                     <div className="feature-card" style={{ width: "20rem" }}>
-                        <h5>HITMAN III</h5>
-                        <p>Stealth Game</p>
+                        <h5>Feature I</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea consectetur quos dolorum.</p>
                         <img src=".." alt="..." />
                     </div>
                     <div className="d-flex flex-row">
                         <div className="feature-card" style={{ width: "20rem" }}>
-                            <h5>HITMAN III</h5>
-                            <p>Stealth Game</p>
+                            <h5>Feature II</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consequuntur nihil ipsum amet!
+                            </p>
                             <img src=".." alt="..." />
                         </div>
                         <div className="d-flex flex-row">
                             <div className="feature-card" style={{ width: "20rem" }}>
-                                <h5>HITMAN III</h5>
-                                <p>Stealth Game</p>
+                                <h5>Feature III</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima distinctio ea quaerat!</p>
                                 <img src=".." alt="..." />
                             </div>
                         </div>
