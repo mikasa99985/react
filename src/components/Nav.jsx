d9defbdd4d4de96b736306c4ce00b749fde23a01
Soubhik <soubhik960@gmail.com>
2023-07-25 21:13:15 +0530

    fix nav, fea

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,14 +9,19 @@ const Nav = () => {
       <nav className="navbar navbar-expand-lg floating-shadow" style={{ backgroundColor: 'rgb(0 0 0)' }} >
         <div className="container-fluid">
 
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            {/* <span className="navbar-toggler-icon"></span> */}
+          {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+              <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+            </svg>
+          </button> */}
+
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
               <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
             </svg>
           </button>
 
-          <div className="d-flex m-auto" style={{cursor:'pointer'}}>
+          <div className="d-flex m-auto" style={{ cursor: 'pointer' }}>
             <img className="anima-icon " src={icon} style={{ width: '40px' }} alt="" />
             <Link className="navbar-brand ms-3 fw-bolder" style={{ color: 'white' }} to="/">
               VIBRANT
@@ -54,7 +59,7 @@ const Nav = () => {
 
 
           <div className="dropdown dropstart">
-            <div className="ms-4 me-3" data-bs-toggle="dropdown" aria-expanded="false" style={{cursor:'pointer'}}>
+            <div className="ms-4 me-3" data-bs-toggle="dropdown" aria-expanded="false" style={{ cursor: 'pointer' }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" className="bi bi-person-circle" viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                 <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
@@ -63,7 +68,7 @@ const Nav = () => {
 
             <ul className="dropdown-menu">
               <li><Link className="dropdown-item" to="/sign_up">Sign up</Link></li>
-              <li><hr className="dropdown-divider"/></li>
+              <li><hr className="dropdown-divider" /></li>
               <li><Link className="dropdown-item" to="/sign_in">Sign in</Link></li>
             </ul>
           </div>
@@ -71,6 +76,18 @@ const Nav = () => {
 
         </div>
       </nav>
+
+      {/* <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Enable body scrolling</button> */}
+
+      <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <p>Try scrolling the rest of the page to see this option in action.</p>
+        </div>
+      </div>
     </>
   );
 };
