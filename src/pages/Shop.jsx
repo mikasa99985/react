a1f7fb8b366705e7c2ea58f553db8d01cb974c17
Akash <debnathakash911@gmail.com>
2023-08-02 21:00:40 +0530

    added shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 export default function Shop() {
   return (
     <div>
-      Hello Shop
+     <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="p-8 rounded-top-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
+    </a>
+    <div class="px-5 pb-5">
+        <a href="#">
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+        </a>
+        <div class="d-flex align-items-center mt-2.5 mb-5">
+            <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+             
+            </svg>
+           
+            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+        </div>
+        <div class="d-flex justify-content-between">
+            <span class="text-3xl font-weight-bold text-gray-900 dark:text-white">$599</span>
+            <a href="#" class="btn btn-primary font-medium rounded-lg text-sm px-5 py-2.5 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        </div>
+    </div>
+</div>
+
     </div>
   )
 }
