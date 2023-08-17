a82d7a50c8818a7347aabe3ba4bb2a5c331c5898
Soubhik <soubhik960@gmail.com>
2023-08-17 10:55:10 +0530

    fix bug in build

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import icon from '../assets/favicon.png'
+import icon from '../assets/favicon.png';
 
 export default function Footer() {
   return (
