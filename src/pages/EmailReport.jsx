41ff39e8ad1a676a86b1c94d4a806b5214f2bff3
Akash <debnathakash911@gmail.com>
2023-08-25 22:19:42 +0530

    edited in Email Report

--- src/pages/EmailReport.jsx
+++ src/pages/EmailReport.jsx
@@ -3,17 +3,25 @@ import React from 'react'
 const EmailReport = () => {
     return (
         <>
-            <div className='container'>
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                    <label for="floatingPassword">Password</label>
-                </div>
-                <button type="button" class="btn">Submit</button>
+            <div class="container">
+                <form class="report-form">
+                    <div class="mb-3">
+                        <label for="floatingInput" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="floatingPassword" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="reportText" class="form-label">Report Description</label>
+                        <textarea class="form-control" id="reportText" rows="4" placeholder="Enter report description"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit Report</button>
+                </form>
             </div>
+
+
         </>
     )
 }
