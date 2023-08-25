638441bc50eb105b07c67612d4e9b07a5e80754c
Akash <debnathakash911@gmail.com>
2023-08-25 14:49:22 +0530

    worked in EmailReport

--- src/pages/EmailReport.jsx
+++ src/pages/EmailReport.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 const EmailReport = () => {
     return (
         <>
-            <div className="m-5">
-                <h1>EmailReport.jsx</h1>
-                <code>test text remove when start work</code>
+            <div className='container'>
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <button type="button" class="btn">Submit</button>
             </div>
         </>
     )
