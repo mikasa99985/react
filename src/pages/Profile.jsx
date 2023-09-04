d8f2bd06cc39fcaa6f6b5da1f2816d34ce0b12b7
Soubhik <soubhik960@gmail.com>
2023-09-04 22:21:36 +0530

    Fix profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { urlDecode } from '../utility/js/util'
-import { db, storageRef } from '../../firebase.config'
+import { db, storageRef, auth } from '../../firebase.config'
 import { Link } from 'react-router-dom'
 import { jsonConcat } from '../utility/js/util'
 
@@ -14,9 +14,13 @@ const Profile = () => {
         email: '',
         phone: '',
         gender: '',
-        age: ''
+        age: '',
+        img: 'https://firebasestorage.googleapis.com/v0/b/online-app-a440d.appspot.com/o/empty-profile.png?alt=media&token=9d884c2e-e9ab-4ac0-9d28-cd7ec5ba917f'
     });
     const [img, setImg] = useState();
+    const [loading, setLoading] = useState(false);
+    const [alert, setAlert] = useState(false);
+    const [isLogin, setLogin] = useState(false);
 
     let name, value;
     const getUserData = (even) => {
@@ -36,16 +40,29 @@ const Profile = () => {
         }).catch((error) => {
             console.log("Error getting document:", error);
         });
+
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setLogin(true);
+            } else {
+                setLogin(false);
+            }
+        });
     }, []);
 
     function submit(e) {
         e.preventDefault();
+        setLoading(true);
         if (img == undefined) {
             // console.log(user);
             db.collection("users").doc(params.id).update(user).then(() => {
-                console.log("Document successfully written!");
+                // console.log("Document successfully written!");
+                setLoading(false);
+                setAlert({ status: true, title: 'Successful!!!', sub_title: 'Document successfully written' });
             }).catch((error) => {
-                console.error("Error writing document: ", error);
+                // console.error("Error writing document: ", error);
+                setLoading(false);
+                setAlert({ status: false, title: 'ERROR!!!', sub_title: 'Some network problem' });
             });
         } else {
             // console.log(jsonConcat(user, {img: 'img'}));
@@ -56,11 +73,12 @@ const Profile = () => {
                 (snapshot) => {
                     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                     // document.getElementById('progressBar').style.width = progress+'%';
-                    console.log(progress, '%');
+                    // console.log(progress, '%');
                 },
                 (error) => {
                     // Handle unsuccessful uploads
-                    console.log(error);
+                    setLoading(false);
+                    setAlert({ status: false, title: 'ERROR!!!', sub_title: 'upload failed' });
                 },
                 () => {
                     // document.getElementById('progress').style.display = 'none';
@@ -72,9 +90,11 @@ const Profile = () => {
                         // console.log(jsonConcat(user, {img: downloadURL}));
 
                         db.collection("users").doc(params.id).update(jsonConcat(user, { img: downloadURL })).then(() => {
-                            console.log("Document successfully written!");
+                            setLoading(false);
+                            setAlert({ status: true, title: 'Successful!!!', sub_title: 'Document successfully written' });
                         }).catch((error) => {
-                            console.error("Error writing document: ", error);
+                            setLoading(false);
+                            setAlert({ status: false, title: 'ERROR!!!', sub_title: 'Some network problem' });
                         });
                     });
                 }
@@ -113,12 +133,21 @@ const Profile = () => {
                             <span className="text-lg text-gray-500">age : {user.age}</span>
                         </div>
                         <div className="mt-4 space-x-6 d-flex">
-                            <button className="btn btn-dark text-lg me-2 d-flex justify-content-center align-items-center" data-bs-toggle="modal" data-bs-target="#EditModal">
-                                Edit
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill ms-2" viewBox="0 0 16 16">
-                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                                </svg>
-                            </button>
+                            {
+                                isLogin ?
+                                    params.id == auth.currentUser.uid ?
+                                        <button className="btn btn-dark text-lg me-2 d-flex justify-content-center align-items-center" data-bs-toggle="modal" data-bs-target="#EditModal">
+                                            Edit
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill ms-2" viewBox="0 0 16 16">
+                                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                                            </svg>
+                                        </button>
+                                        :
+                                        <></>
+                                    :
+                                    <></>
+
+                            }
                             <Link to="/" className="btn btn-secondary text-lg d-flex justify-content-center align-items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-left-fill me-1" viewBox="0 0 16 16">
                                     <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
@@ -169,10 +198,25 @@ const Profile = () => {
                                     <input type="number" className="form-control" id="age" name='age' value={user.age} onChange={getUserData} />
                                 </div>
 
-                                <div className="modal-footer d-flex justify-content-between">
-                                    <div className="spinner-border" role="status">
-                                        <span className="visually-hidden">Loading...</span>
-                                    </div>
+                                {
+                                    alert ?
+                                        <div class={`alert ${alert ? 'alert-success' : 'alert-danger'} alert-dismissible fade show`} role="alert">
+                                            <strong>{alert.title}</strong> {alert.sub_title}
+                                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                        </div>
+                                        :
+                                        <></>
+                                }
+
+                                <div className={`modal-footer d-flex ${loading ? 'justify-content-between' : 'justify-content-end'}`}>
+                                    {
+                                        loading ?
+                                            <div className="spinner-border" role="status">
+                                                <span className="visually-hidden">Loading...</span>
+                                            </div>
+                                            :
+                                            <></>
+                                    }
                                     <div>
                                         <button type="button" className="btn btn-secondary me-2" data-bs-dismiss="modal">Close</button>
                                         <button type="submit" className="btn btn-primary">Save changes</button>
