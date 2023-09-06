614f7217651968c846df87e4bd872dcb33c4d6a9
Soubhik <soubhik960@gmail.com>
2023-09-06 14:17:14 +0530

    fix some bug

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -10,6 +10,7 @@ export default function Shop() {
 
     const [list, setList] = useState([]);
     const [isLogin, setLogin] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffectOnce(() => {
         let query_db;
@@ -36,29 +37,31 @@ export default function Shop() {
                             arr.push(parseInt(Childsnapshot.val().rate));
                         });
                 }).then(() => {
-                    setList(oldArray => [...oldArray, { doc: doc, rate: arr }])
+                    setList(oldArray => [...oldArray, { doc: doc, rate: arr }]);
                 });
 
             });
+        }).then(()=>{
+            setLoading(false);
         }).catch((error) => {
             console.log("Error getting documents: ", error);
         });
 
     }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         auth.onAuthStateChanged((user) => {
             if (user) {
                 setLogin(true);
             } else {
-               setLogin(false);
+                setLogin(false);
             }
         });
-    },[]);
+    }, []);
 
     async function addCard(key, name, price, img, offers) {
         if (isLogin) {
-            await database.ref(`users/${auth.currentUser.uid}/my_cards/${key}`).set({name, price:calculateDiscountedPrice(price,offers), img});
+            await database.ref(`users/${auth.currentUser.uid}/my_cards/${key}`).set({ name, price: calculateDiscountedPrice(price, offers), img });
             return true;
         } else {
             return false;
@@ -71,31 +74,34 @@ export default function Shop() {
 
             <div className="d-flex flex-wrap justify-content-center mx-auto w-res my-3">
                 {
-                    list.length == 0 ?
-                        <div className=" d-flex justify-content-around align-items-center" style={{height:'calc(100vh - 88px)'}}>
-                            <div className="spinner-border" style={{width:'70px',height:'70px', borderWidth:'8px'}} role="status">
+                    loading ?
+                        <div className=" d-flex justify-content-around align-items-center" style={{ height: 'calc(100vh - 88px)' }}>
+                            <div className="spinner-border" style={{ width: '70px', height: '70px', borderWidth: '8px' }} role="status">
                                 <span className="visually-hidden">Loading...</span>
                             </div>
                         </div>
                         :
-                        list.map((element, index) => {
-                            return (
-                                <ShopCard
-                                    id={element.doc.id}
-                                    img={element.doc.data().img}
-                                    title={element.doc.data().name.length >= 25 ? element.doc.data().name.slice(0, 25) + '...' : element.doc.data().name}
-                                    name={element.doc.data().name}
-                                    price={element.doc.data().price == 0 ? 'FREE' : '₹' + calculateDiscountedPrice(element.doc.data().price, element.doc.data().offers)}
-                                    row_price={element.doc.data().price}
-                                    rate={calculateMean(element.rate)}
-                                    offers={element.doc.data().offers}
-                                    key={index}
-                                    link={`/shop/${urlEncode(element.doc.data().name)}`}
-                                    // addCard={()=>{addCard(element.doc.id, element.doc.data().name, element.doc.data().img)}}
-                                    addCard={addCard}
-                                />
-                            )
-                        })
+                        list.length != 0 ?
+                            list.map((element, index) => {
+                                return (
+                                    <ShopCard
+                                        id={element.doc.id}
+                                        img={element.doc.data().img}
+                                        title={element.doc.data().name.length >= 25 ? element.doc.data().name.slice(0, 25) + '...' : element.doc.data().name}
+                                        name={element.doc.data().name}
+                                        price={element.doc.data().price == 0 ? 'FREE' : '₹' + calculateDiscountedPrice(element.doc.data().price, element.doc.data().offers)}
+                                        row_price={element.doc.data().price}
+                                        rate={calculateMean(element.rate)}
+                                        offers={element.doc.data().offers}
+                                        key={index}
+                                        link={`/shop/${urlEncode(element.doc.data().name)}`}
+                                        // addCard={()=>{addCard(element.doc.id, element.doc.data().name, element.doc.data().img)}}
+                                        addCard={addCard}
+                                    />
+                                )
+                            })
+                            :
+                            <h1>no data found</h1>
                 }
             </div>
         </>
