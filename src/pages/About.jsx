c50cd020741401537d7dedae443b223db1c2fdff
Akash <debnathakash911@gmail.com>
2023-09-13 22:15:45 +0530

    made about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,58 @@ const About = () => {
   return (
     <>
       <h1>About</h1>
+    
+
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Your Website</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="about.html">About Us <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+ 
+    <section class="container mt-5">
+        <div class="row">
+            <div class="col-lg-6" >
+                <h2>About Us</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu sem sit amet ligula
+                    blandit bibendum a et elit. Nulla facilisi. Vivamus laoreet mi a mi pretium, a sollicitudin
+                    risus tincidunt.
+                </p>
+            </div>
+            <div class="col-lg-6">
+                <img src="about-image.jpg" alt="About Us" class="img-fluid"/>
+            </div>
+        </div>
+    </section>
+
+    <footer class="bg-dark text-white text-center py-3">
+        <p>&copy; 2023 Your Website. All Rights Reserved.</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
     </>
+
+
   )
 }
 
