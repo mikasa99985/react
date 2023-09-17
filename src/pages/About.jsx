e3b2c7a7e9a26bd93251555e9e862a43d9c27f56
Akash <debnathakash911@gmail.com>
2023-09-17 21:27:25 +0530

    made class into className

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,52 +4,53 @@ const About = () => {
   return (
     <>
       <h1>About</h1>
-      <div class="container text-center">
-        <div class="hero-text mw-custom-m ml-auto mr-auto ph3">
-          <h1 class="ubisoftsans tc white">ABOUT US</h1>
-          <p class="ubisoftsans tc white f2 b">Enrich players' lives by creating memorable and meaningful gaming experiences</p>
-          <div class="container">
-            <div class="row justify-content-end">
-              <div class="col-lg-6">
+      <div className="container text-center">
+        <div className="hero-text mw-custom-m ml-auto mr-auto ph3">
+          <h1 classNameName="ubisoftsans tc white">ABOUT US</h1>
+          <p className="ubisoftsans tc white f2 b">Enrich players' lives by creating memorable and meaningful gaming experiences</p>
+          <div className="container">
+            <div className="row justify-content-end">
+              <div className="col-lg-6">
                 
-              <div class="mt-5 feature-card bg-near-white pa3">
-    <h2 class="mt0 mb3">Our Story</h2>
-    <p class="lh-copy mb3">Ubisoft may have 21,000 employees in offices and studios around the world, but the company began with a family. </p>
-    <a path="/about-us/our-story" aria-label="Learn More: Our Story" class="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-story">
+              <div className="mt-5 feature-card bg-near-white pa3">
+
+    <h2 className="mt0 mb3">Our Story</h2>
+    <p className="lh-copy mb3">Ubisoft may have 21,000 employees in offices and studios around the world, but the company began with a family. </p>
+    <a path="/about-us/our-story" aria-label="Learn More: Our Story" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-story">
         <span>Learn More</span>
     </a>
 </div>
 
               </div>
             </div>
-            <div class="feature-card bg-near-white pa3"><h2 class="mt0 mb3">Our Brands</h2><p class="lh-copy mb3">At Ubisoft, we create worlds for everyone. We are dedicated to enriching the lives of our players by developing high-quality games that resonate with all kinds of personalities, bring people together, and allow everyone to learn and grow while having fun.</p><a path="/about-us/our-brands" aria-label="Learn More: Our Brands" class="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-brands"><span>Learn More</span></a></div>
-            <div class="container">
-              <div class="row justify-content-end">
-                <div class="col-lg-6">
-                  <div class="feature-card bg-near-white pa3">
-                    <h2 class="mt0 mb3">Our People</h2>
-                    <p class="lh-copy mb3">Discover the exciting stories of the people behind our games.</p>
-                    <a path="/about-us/our-people" aria-label="Learn More: Our People" class="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-people">
+            <div className="feature-card bg-near-white pa3"><h2 className="mt0 mb3">Our Brands</h2><p className="lh-copy mb3">At Ubisoft, we create worlds for everyone. We are dedicated to enriching the lives of our players by developing high-quality games that resonate with all kinds of personalities, bring people together, and allow everyone to learn and grow while having fun.</p><a path="/about-us/our-brands" aria-label="Learn More: Our Brands" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-brands"><span>Learn More</span></a></div>
+            <div className="container">
+              <div className="row justify-content-end">
+                <div className="col-lg-6">
+                  <div className="feature-card bg-near-white pa3">
+                    <h2 className="mt0 mb3">Our People</h2>
+                    <p className="lh-copy mb3">Discover the exciting stories of the people behind our games.</p>
+                    <a path="/about-us/our-people" aria-label="Learn More: Our People" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-people">
                       <span>Learn More</span>
                     </a>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="feature-card bg-near-white pa3"><h2 class="mt0 mb3">Diversity and Inclusion</h2><p class="lh-copy mb3">Learn more about how we are putting D&amp;I at the heart of everything we do with a focus on colleagues, content, culture and community.</p><a path="/about-us/diversity-inclusion" aria-label="Learn More: Diversity and Inclusion" class="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/diversity-inclusion"><span>Learn More</span></a></div>
-            <div class="container">
-              <div class="row justify-content-end">
-                <div class="col-lg-6">
-                  <div class="feature-card bg-near-white pa3">
-                    <h2 class="mt0 mb3">Investor Center</h2>
-                    <p class="lh-copy mb3">The Investor Center page centralizes detailed information for the company’s shareholders and prospective investors and provides plenty of documentation...</p>
-                    <a path="/about-us/investors" aria-label="Learn More: Investor Center" class="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/investors">
+            <div className="feature-card bg-near-white pa3"><h2 className="mt0 mb3">Diversity and Inclusion</h2><p className="lh-copy mb3">Learn more about how we are putting D&amp;I at the heart of everything we do with a focus on colleagues, content, culture and community.</p><a path="/about-us/diversity-inclusion" aria-label="Learn More: Diversity and Inclusion" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/diversity-inclusion"><span>Learn More</span></a></div>
+            <div className="container">
+              <div className="row justify-content-end">
+                <div className="col-lg-6">
+                  <div className="feature-card bg-near-white pa3">
+                    <h2 className="mt0 mb3">Investor Center</h2>
+                    <p className="lh-copy mb3">The Investor Center page centralizes detailed information for the company’s shareholders and prospective investors and provides plenty of documentation...</p>
+                    <a path="/about-us/investors" aria-label="Learn More: Investor Center" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/investors">
                       <span>Learn More</span>
                     </a>
                   </div>
                 </div>
               </div>
-              <div class="feature-card bg-near-white pa3"><h2 class="mt0 mb3">Ubisoft Codes of Conduct</h2><p class="lh-copy mb3">Whether they apply to our employees, our players, or our business partners, Ubisoft's Codes of Conduct are reference documents that define our commitments as a company and game publisher.</p><a path="/about-us/codes-of-conduct" aria-label="Learn More: Ubisoft Codes of Conduct" class="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/codes-of-conduct"><span>Learn More</span></a></div>
+              <div className="feature-card bg-near-white pa3"><h2 className="mt0 mb3">Ubisoft Codes of Conduct</h2><p className="lh-copy mb3">Whether they apply to our employees, our players, or our business partners, Ubisoft's Codes of Conduct are reference documents that define our commitments as a company and game publisher.</p><a path="/about-us/codes-of-conduct" aria-label="Learn More: Ubisoft Codes of Conduct" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/codes-of-conduct"><span>Learn More</span></a></div>
             </div>
 
           </div>
