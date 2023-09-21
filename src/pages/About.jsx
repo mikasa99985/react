704db7929de3ac3a075925ebd5727618cf2c4e60
Akash <debnathakash911@gmail.com>
2023-09-21 21:03:18 +0530

    added pics in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Banner from '../components/banner';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
 
 const About = () => {
   return (
@@ -13,8 +10,10 @@ const About = () => {
           <p className="ubisoftsans tc white f2 b">Enrich players' lives by creating memorable and meaningful gaming experiences</p>
           <div className="container">
             <div className="row justify-content-end">
+               <div className="col-lg-6"> {/* New column for the image */}
+    <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
+  </div>
               <div className="col-lg-6">
-                
               <div className="mt-5 feature-card bg-near-white pa3">
 
     <h2 className="mt0 mb3">Our Story</h2>
@@ -26,25 +25,40 @@ const About = () => {
 
               </div>
             </div>
-            <div className="feature-card bg-near-white pa3"><h2 className="mt0 mb3">Our Brands</h2><p className="lh-copy mb3">At Ubisoft, we create worlds for everyone. We are dedicated to enriching the lives of our players by developing high-quality games that resonate with all kinds of personalities, bring people together, and allow everyone to learn and grow while having fun.</p><a path="/about-us/our-brands" aria-label="Learn More: Our Brands" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-brands"><span>Learn More</span></a></div>
+            <div class="feature-row mb5-m mb6 cover-right"><div class="feature-cover"><div class="feature-cover-img" ><img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image"/></div></div><div class="feature-card bg-near-white pa3"><h2 class="mt0 mb3">Our Brands</h2><p class="lh-copy mb3">At Ubisoft, we create worlds for everyone. We are dedicated to enriching the lives of our players by developing high-quality games that resonate with all kinds of personalities, bring people together, and allow everyone to learn and grow while having fun.</p><a path="/about-us/our-brands" aria-label="Learn More: Our Brands" class="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-brands"><span>Learn More</span></a></div></div>
             <div className="container">
               <div className="row justify-content-end">
+                <div className="feature-row mb5-m mb6 cover-left">
+                <div className="feature-cover">
+                <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
+                </div>
+                </div>
                 <div className="col-lg-6">
                   <div className="feature-card bg-near-white pa3">
                     <h2 className="mt0 mb3">Our People</h2>
                     <p className="lh-copy mb3">Discover the exciting stories of the people behind our games.</p>
                     <a path="/about-us/our-people" aria-label="Learn More: Our People" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-people">
                       <span>Learn More</span>
+                      
                     </a>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="feature-cover"></div>
+            <div className="feature-cover">
+                <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
+                </div>
             <div className="feature-card bg-near-white pa3"><h2 className="mt0 mb3">Diversity and Inclusion</h2><p className="lh-copy mb3">Learn more about how we are putting D&amp;I at the heart of everything we do with a focus on colleagues, content, culture and community.</p><a path="/about-us/diversity-inclusion" aria-label="Learn More: Diversity and Inclusion" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/diversity-inclusion"><span>Learn More</span></a></div>
             <div className="container">
               <div className="row justify-content-end">
+              <div className="feature-cover">
+                <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
+                </div>
                 <div className="col-lg-6">
+                  
                   <div className="feature-card bg-near-white pa3">
+                    
                     <h2 className="mt0 mb3">Investor Center</h2>
                     <p className="lh-copy mb3">The Investor Center page centralizes detailed information for the company’s shareholders and prospective investors and provides plenty of documentation...</p>
                     <a path="/about-us/investors" aria-label="Learn More: Investor Center" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/investors">
@@ -53,15 +67,20 @@ const About = () => {
                   </div>
                 </div>
               </div>
+              <div className="feature-cover">
+                <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
+                </div>
               <div className="feature-card bg-near-white pa3"><h2 className="mt0 mb3">Ubisoft Codes of Conduct</h2><p className="lh-copy mb3">Whether they apply to our employees, our players, or our business partners, Ubisoft's Codes of Conduct are reference documents that define our commitments as a company and game publisher.</p><a path="/about-us/codes-of-conduct" aria-label="Learn More: Ubisoft Codes of Conduct" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/codes-of-conduct"><span>Learn More</span></a></div>
             </div>
 
           </div>
+      
         </div>
-
       </div>
 
-      <Footer/>
+
+
+
 
     </>
 
