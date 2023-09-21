654520725053fb617fb103a62045522780ec4cfe
Debjit Roy <debjitroy515@gmail.com>
2023-09-21 20:54:44 +0530

    bot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
 import icon from '../assets/fav_icon.png';
 import { auth, database } from '../../firebase.config';
-import '../utility/css/footer.css' ;
+import '../utility/css/footer.css';
 import bot_img from '../assets/Botlogo.svg';
 import Slack from '../assets/Slack.png'
 import Chatbanner from "../assets/Chatbanner.png"
@@ -124,28 +124,39 @@ export default function Footer() {
 
       <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog modal-dialog-centered modal-cos">
-          <div className="modal-content mb-4" style={{ height: "65vh", width: "58vh", background: "white" , right:"2vh", top:"2vh" }}>
-            <div className="modal-header" style={{ height: "7vh" , backgroundImage:`url(${Chatbanner})`  }} >
+          <div className="modal-content mb-4" style={{ height: "65vh", width: "58vh", background: "white", right: "2vh", top: "2vh" }}>
+            <div className="modal-header" style={{ height: "7vh", backgroundImage: `url(${Chatbanner})` }} >
               <img src={Slack} width={28} alt="" />
               {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-apple" viewBox="0 0 16 16">
                 <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
                 <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
               </svg> */}
               <div className="modal-title fs-5" id="exampleModalLabel" style={{ right: "5px" }}> </div>
-              
+
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div className="modal-body" style={{padding:"0px", backgroundImage:`url(${Chatbody})` }} >
-            
-            {/**394.59  295.5 */}
+            <div className="modal-body" style={{ padding: "0px", backgroundImage: `url(${Chatbody})` }} >
+
+              {/**394.59  295.5 */}
 
             </div>
-            <div className="modal-footer "  >
-            <button type="button" className={`btn btn-primary rgbbutton1 ${zoomClass}`} onMouseEnter={() => setIsZoomed(true)} onMouseLeave={() => setIsZoomed(false)} style={{ width: "7vh", height: "7vh", left: "8px", top:"7px" , display: "flex", alignItems: "center", position:"relative" , justifyContent: "center", borderRadius: "50px", background: "black" }} >  send  </button>
+            <div className="chatfooter" style={{ height: "10vh" }}  >
+
+              
+
+                <button type="button" className={`btn btn-primary rgbbutton1 ${zoomClass}`} onMouseEnter={() => setIsZoomed(true)} onMouseLeave={() => setIsZoomed(false)} style={{ width: "5vh", height: "5vh", left: "50vh", top: "17px", alignItems: "center", position: "relative", justifyContent: "center", borderRadius: "15px", background: "pink", borderColor: "pink" }} >
+
+                  <div className='test' style={{ scale: "0.4", display: "flex", justifyContent: "center", alignItems: "center", height: "10vh", width: "10vh", right: "30px", bottom: "23px", position: "relative" }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" style={{ height: "6vh", width: "6vh" }} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>
+                  </div>
+
+
+                </button>
+              
             </div>
           </div>
         </div>
       </div>
     </>
-    )
+  )
 }
