10845e48df15f1829ad8550ce3c3517502ea054a
Soubhik <soubhik960@gmail.com>
2023-09-21 21:41:24 +0530

    fix about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,86 +1,86 @@
 import React from 'react'
+import Banner from '../components/banner';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 
 const About = () => {
   return (
     <>
-      <h1>About</h1>
-      <div className="container text-center">
-        <div className="hero-text mw-custom-m ml-auto mr-auto ph3">
-          <h1 classNameName="ubisoftsans tc white">ABOUT US</h1>
-          <p className="ubisoftsans tc white f2 b">Enrich players' lives by creating memorable and meaningful gaming experiences</p>
-          <div className="container">
-            <div className="row justify-content-end">
-               <div className="col-lg-6"> {/* New column for the image */}
-    <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
-  </div>
-              <div className="col-lg-6">
-              <div className="mt-5 feature-card bg-near-white pa3">
+      {/* <h1>About</h1> */}
 
-    <h2 className="mt0 mb3">Our Story</h2>
-    <p className="lh-copy mb3">Ubisoft may have 21,000 employees in offices and studios around the world, but the company began with a family. </p>
-    <a path="/about-us/our-story" aria-label="Learn More: Our Story" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-story">
-        <span>Learn More</span>
-    </a>
-</div>
+      <Nav />
 
-              </div>
-            </div>
-            <div class="feature-row mb5-m mb6 cover-right"><div class="feature-cover"><div class="feature-cover-img" ><img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image"/></div></div><div class="feature-card bg-near-white pa3"><h2 class="mt0 mb3">Our Brands</h2><p class="lh-copy mb3">At Ubisoft, we create worlds for everyone. We are dedicated to enriching the lives of our players by developing high-quality games that resonate with all kinds of personalities, bring people together, and allow everyone to learn and grow while having fun.</p><a path="/about-us/our-brands" aria-label="Learn More: Our Brands" class="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-brands"><span>Learn More</span></a></div></div>
-            <div className="container">
-              <div className="row justify-content-end">
-                <div className="feature-row mb5-m mb6 cover-left">
-                <div className="feature-cover">
-                <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
-                </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="feature-card bg-near-white pa3">
-                    <h2 className="mt0 mb3">Our People</h2>
-                    <p className="lh-copy mb3">Discover the exciting stories of the people behind our games.</p>
-                    <a path="/about-us/our-people" aria-label="Learn More: Our People" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/our-people">
-                      <span>Learn More</span>
-                      
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="feature-cover"></div>
-            <div className="feature-cover">
-                <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
-                </div>
-            <div className="feature-card bg-near-white pa3"><h2 className="mt0 mb3">Diversity and Inclusion</h2><p className="lh-copy mb3">Learn more about how we are putting D&amp;I at the heart of everything we do with a focus on colleagues, content, culture and community.</p><a path="/about-us/diversity-inclusion" aria-label="Learn More: Diversity and Inclusion" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/diversity-inclusion"><span>Learn More</span></a></div>
-            <div className="container">
-              <div className="row justify-content-end">
-              <div className="feature-cover">
-                <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
-                </div>
-                <div className="col-lg-6">
-                  
-                  <div className="feature-card bg-near-white pa3">
-                    
-                    <h2 className="mt0 mb3">Investor Center</h2>
-                    <p className="lh-copy mb3">The Investor Center page centralizes detailed information for the company’s shareholders and prospective investors and provides plenty of documentation...</p>
-                    <a path="/about-us/investors" aria-label="Learn More: Investor Center" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/investors">
-                      <span>Learn More</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="feature-cover">
-                <img src="https://www.designinfo.in/wp-content/uploads/nc/p/5/2/1/6/3/52163-364x364-optimized.jpg" alt="Description of the image" />
-                </div>
-              <div className="feature-card bg-near-white pa3"><h2 className="mt0 mb3">Ubisoft Codes of Conduct</h2><p className="lh-copy mb3">Whether they apply to our employees, our players, or our business partners, Ubisoft's Codes of Conduct are reference documents that define our commitments as a company and game publisher.</p><a path="/about-us/codes-of-conduct" aria-label="Learn More: Ubisoft Codes of Conduct" className="f5 br2 link dim br-pill ls-md ba bw1 b--primary-magenta ph4 pv2 dib white bg-primary-magenta ttu b pointer" locale="en-us" href="/en-us/company/about-us/codes-of-conduct"><span>Learn More</span></a></div>
-            </div>
+      <Banner height='60vh' img='https://pbs.twimg.com/media/FXMBpDaXoAY8isj?format=jpg&name=4096x4096'>
+        <h1 className="fw-bolder mb-5">ABOUT US</h1>
+        <h1>Enrich players' lives by creating memorable and meaningful gaming experiences</h1>
+      </Banner>
 
+      <div className="container my-5">
+
+        <div class="left row featurette my-5">
+          <div class="col-md-5 order-md-2 d-flex flex-column justify-content-center">
+            <h1 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h1>
+            <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
           </div>
-      
+          <div class="col-md-7 order-md-1">
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="800" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">800x500</text></svg>
+          </div>
         </div>
+
+        <div class="right row featurette my-5">
+          <div class="col-md-5 d-flex flex-column justify-content-center">
+            <h1 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-body-secondary">It’ll blow your mind.</span></h1>
+            <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+          </div>
+          <div class="col-md-7">
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="800" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">800x500</text></svg>
+          </div>
+        </div>
+        {/* fix */}
+
+        <div class="left row featurette my-5">
+          <div class="col-md-5 order-md-2 d-flex flex-column justify-content-center">
+            <h1 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h1>
+            <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+          </div>
+          <div class="col-md-7 order-md-1">
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="800" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">800x500</text></svg>
+          </div>
+        </div>
+
+        <div class="right row featurette my-5">
+          <div class="col-md-5 d-flex flex-column justify-content-center">
+            <h1 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-body-secondary">It’ll blow your mind.</span></h1>
+            <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+          </div>
+          <div class="col-md-7">
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="800" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">800x500</text></svg>
+          </div>
+        </div>
+
+        <div class="left row featurette my-5">
+          <div class="col-md-5 order-md-2 d-flex flex-column justify-content-center">
+            <h1 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h1>
+            <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+          </div>
+          <div class="col-md-7 order-md-1">
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="800" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">800x500</text></svg>
+          </div>
+        </div>
+
+        <div class="right row featurette my-5">
+          <div class="col-md-5 d-flex flex-column justify-content-center">
+            <h1 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-body-secondary">It’ll blow your mind.</span></h1>
+            <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+          </div>
+          <div class="col-md-7">
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="800" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">800x500</text></svg>
+          </div>
+        </div>
+
       </div>
 
-
-
-
+      <Footer/>
 
     </>
 
