afc21c709215864f27dfbc4e6fd4233bae4cf062
Debjit Roy <debjitroy515@gmail.com>
2023-09-27 16:10:45 +0530

    s

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -149,7 +149,7 @@ export default function Footer() {
                 
               </div> */}
 
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" style={{}} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className='svgbutton' width="30" height="30" style={{}} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>
 
 
               {/* </button> */}
