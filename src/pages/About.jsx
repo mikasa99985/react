e8d8f65fdda0eee18d4bcb0ee81697fec6b6c8ca
Akash <debnathakash911@gmail.com>
2023-09-28 22:09:27 +0530

    added image in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
             <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
           </div>
           <div class="col-md-7 order-md-1">
-            <img src="https://emle.org/wp-content/uploads/revslider/news/demo-img-2-3.png" width={670} alt="" />
+            <img src="https://img.freepik.com/premium-photo/gaming-desktop-pc-computer-setup-gamer-illustration_691560-5795.jpg" width={670} alt="" />
           </div>
         </div>
 
@@ -33,7 +33,7 @@ const About = () => {
             <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
           </div>
           <div class="col-md-7">
-            <img src="https://emle.org/wp-content/uploads/revslider/news/demo-img-2-3.png" width={670} alt="" />
+            <img src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8yN19waG90b19vZl9idXNpbmVzc3dvbWFuX3dpdGhfaGVhZHBob25lc19zbWlsaV84ZmUwOWM5OS05NjhmLTRmNDEtOWRkMC1kMjMwNWFkMDFkMzNfMi5qcGc.jpg" width={670} alt="" />
           </div>
         </div>
         {/* fix */}
@@ -44,7 +44,7 @@ const About = () => {
             <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
           </div>
           <div class="col-md-7 order-md-1">
-            <img src="https://emle.org/wp-content/uploads/revslider/news/demo-img-2-3.png" width={670} alt="" />
+            <img src="https://previews.123rf.com/images/parilovv/parilovv2001/parilovv200100875/138472206-professional-gamers-cafe-room-with-powerful-personal-computer-game-chair-blue-color-concept-cyber.jpg" width={670} alt="" />
           </div>
         </div>
 
@@ -54,7 +54,7 @@ const About = () => {
             <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
           </div>
           <div class="col-md-7">
-            <img src="https://emle.org/wp-content/uploads/revslider/news/demo-img-2-3.png" width={670} alt="" />
+            <img src="https://thumbs.dreamstime.com/b/happy-professional-cyber-sport-gamer-playing-online-computer-game-headphones-blurred-red-blue-background-196931002.jpg" width={670} alt="" />
           </div>
         </div>
 
@@ -64,7 +64,7 @@ const About = () => {
             <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
           </div>
           <div class="col-md-7 order-md-1">
-            <img src="https://emle.org/wp-content/uploads/revslider/news/demo-img-2-3.png" width={670} alt="" />
+            <img src="https://c4.wallpaperflare.com/wallpaper/162/894/557/neon-keyboards-computer-pc-gaming-wallpaper-preview.jpg" width={670} alt="" />
           </div>
         </div>
 
@@ -74,7 +74,7 @@ const About = () => {
             <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
           </div>
           <div class="col-md-7">
-            <img src="https://emle.org/wp-content/uploads/revslider/news/demo-img-2-3.png" width={670} alt="" />
+            <img src="https://media.istockphoto.com/id/1387529248/photo/portrait-of-a-teenage-boy-playing-on-the-computer-at-home.jpg?s=612x612&w=0&k=20&c=WpQge8N6xINYshRZZMWTIf8a44fPBqMou4FG-nOaBMo=" width={670} alt="" />
           </div>
         </div>
 
