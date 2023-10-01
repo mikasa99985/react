462b85ab62315a309f30054e0bc787aa4cb79720
Soubhik <soubhik960@gmail.com>
2023-10-01 17:28:34 +0530

    Add components/details/imgslider.jsx

---
+++ src/components/details/Imgslider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Imgslider = () => {
+  return (
+    <>
+      {/* Create image slider here */}
+      <h4>components/details/Imgslider.jsx</h4>  {/* <--- replace this code */}
+    </>
+  )
+}
+
+export default Imgslider
