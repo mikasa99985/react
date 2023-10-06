c15c1e547f886cf0f2a36f57fe61c650276d10e6
Soumyadipta <aourkopalbag@gmail.com>
2023-10-06 07:50:32 +0530

    imageslider updated

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -3,82 +3,105 @@ import icon from '../assets/right-arrow.png'
 export const ImageSlider = () => {
   return (
 
-<>
-  <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <img src="https://cdn.sanity.io/images/6znhzi10/production/e58fe693e62a07c633b48885817b374c1e047078-2400x1350.jpg?w=1600" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="img-slider-indi active" aria-current="true" aria-label="Slide 1"></img>
-    <img src="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/07/Spider-Man-Remastered-PC.jpg" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" className='img-slider-indi' aria-label="Slide 2"></img>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrg85UaJB1LbsSBZW4mOGAB7k1mZUA21UwBg&usqp=CAU" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" className='img-slider-indi' aria-label="Slide 3"></img>
-  </div>
-  <div class="carousel-inner">
+    <>
+      <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <img src="https://cdn.sanity.io/images/6znhzi10/production/e58fe693e62a07c633b48885817b374c1e047078-2400x1350.jpg?w=1600" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="img-slider-indi active" aria-current="true" aria-label="Slide 1"></img>
 
-    <div class="carousel-item d-flex justify-content-center active">
-      <img src="https://cdn.sanity.io/images/6znhzi10/production/e58fe693e62a07c633b48885817b374c1e047078-2400x1350.jpg?w=1600" class="d-block slider-img-size" alt="..."/>
-      
-      <div class="img-slider-text-container">
-        <div className="img-slider-text">
-          <div className="s-text">
-            <h1>JOIN THE CLOSED BETA!</h1>
-            <h5>Enter the perilous paradise of Skull and Bones during the Closed Beta running from August 25th to August 28th</h5>
-          </div>
-          <div className="s-button mt-2 mt-sm-3 mt-lg-5">
-            <button className="btn-cus btn-radius btn-blue-glow s-btn-cus me-md-5 mb-1 mb-sm-4 mb-md-0" >VISIT WEBSITE</button>
-            <button className="btn-cus btn-radius btn-blue-glow s-btn-cus" >BUY NOW</button>
-          </div>
+          <img src="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/07/Spider-Man-Remastered-PC.jpg" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" className='img-slider-indi' aria-label="Slide 2"></img>
+
+          <img src="https://wallpapercave.com/wp/wp9351526.jpg" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" className='img-slider-indi' aria-label="Slide 3"></img>
+
+          
+          <img src="https://cdn.wallpapersafari.com/29/27/NeCdV8.png" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" className='img-slider-indi' aria-label="Slide 4"></img>
         </div>
-      </div>
+        <div class="carousel-inner">
 
-    </div>
+          <div class="carousel-item d-flex justify-content-center active">
+            <img src="https://cdn.sanity.io/images/6znhzi10/production/e58fe693e62a07c633b48885817b374c1e047078-2400x1350.jpg?w=1600" class="d-block slider-img-size" alt="..." />
 
-    <div class="carousel-item d-flex justify-content-center">
-      <img src="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/07/Spider-Man-Remastered-PC.jpg" class="d-block slider-img-size" alt="..."/>
-      
-      <div class="img-slider-text-container">
-        <div className="img-slider-text">
-          <div className="s-text">
-            <h1>OPERATION HEAVY METTLE</h1>
-            <h5>Our newest Attacker, Ram, comes to us from South Korea and is ready to join the ranks of Redhammer.</h5>
+            <div class="img-slider-text-container">
+              <div className="img-slider-text">
+                <div className="s-text">
+                  <h1>JOIN THE CLOSED BETA!</h1>
+                  <h5>Enter the perilous paradise of Skull and Bones during the Closed Beta running from August 25th to August 28th</h5>
+                </div>
+                <div className="s-button mt-2 mt-sm-3 mt-lg-5">
+                  <button className="btn-cus btn-radius btn-blue-glow s-btn-cus me-md-5 mb-1 mb-sm-4 mb-md-0" >VISIT WEBSITE</button>
+                  <button className="btn-cus btn-radius btn-blue-glow s-btn-cus" >BUY NOW</button>
+                </div>
+              </div>
+            </div>
+
           </div>
-          <div className="s-button mt-2 mt-sm-3 mt-lg-5">
-            <button className="btn-cus btn-radius btn-blue-glow s-btn-cus me-md-5 mb-1 mb-sm-4 mb-md-0" >VISIT WEBSITE</button>
-            <button className="btn-cus btn-radius btn-blue-glow s-btn-cus" >BUY NOW</button>
+
+          <div class="carousel-item d-flex justify-content-center">
+            <img src="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/07/Spider-Man-Remastered-PC.jpg" class="d-block slider-img-size" alt="..." />
+
+            <div class="img-slider-text-container">
+              <div className="img-slider-text">
+                <div className="s-text">
+                  <h1>OPERATION HEAVY METTLE</h1>
+                  <h5>Our newest Attacker, Ram, comes to us from South Korea and is ready to join the ranks of Redhammer.</h5>
+                </div>
+                <div className="s-button mt-2 mt-sm-3 mt-lg-5">
+                  <button className="btn-cus btn-radius btn-blue-glow s-btn-cus me-md-5 mb-1 mb-sm-4 mb-md-0" >VISIT WEBSITE</button>
+                  <button className="btn-cus btn-radius btn-blue-glow s-btn-cus" >BUY NOW</button>
+                </div>
+              </div>
+            </div>
+
           </div>
+
+          <div class="carousel-item d-flex justify-content-center">
+            <img src="https://wallpapercave.com/wp/wp9351526.jpg" class="d-block slider-img-size" alt="..." />
+
+            <div class="img-slider-text-container">
+              <div className="img-slider-text">
+                <div className="s-text">
+                  <h1>AUTUMN SALE - UP TO 75% OFF!</h1>
+                  <h5>Embark on the gaming season ahead.</h5>
+                </div>
+                <div className="s-button mt-2 mt-sm-3 mt-lg-5">
+                  <button className="btn-cus btn-radius btn-blue-glow s-btn-cus me-md-5 mb-1 mb-sm-4 mb-md-0" >VISIT WEBSITE</button>
+                  <button className="btn-cus btn-radius btn-blue-glow s-btn-cus" >BUY NOW</button>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="carousel-item d-flex justify-content-center">
+            <img src="https://cdn.wallpapersafari.com/29/27/NeCdV8.png" class="d-block slider-img-size" alt="..." />
+
+            <div class="img-slider-text-container">
+              <div className="img-slider-text">
+                <div className="s-text">
+                  <h1>AUTUMN SALE - UP TO 75% OFF!</h1>
+                  <h5>Embark on the gaming season ahead.</h5>
+                </div>
+                <div className="s-button mt-2 mt-sm-3 mt-lg-5">
+                  <button className="btn-cus btn-radius btn-blue-glow s-btn-cus me-md-5 mb-1 mb-sm-4 mb-md-0" >VISIT WEBSITE</button>
+                  <button className="btn-cus btn-radius btn-blue-glow s-btn-cus" >BUY NOW</button>
+                </div>
+              </div>
+              </div>
+
+            </div>
+
+
         </div>
+        <button class="carousel-control-prev" style={{ top: 'unset', bottom: '80px' }} type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          {/* <span class="carousel-control-prev-icon" aria-hidden="true"></span> */}
+          <img src={icon} style={{ rotate: "180deg" }} width={40} alt="btn" />
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" style={{ top: 'unset', bottom: '80px' }} type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          {/* <span class="carousel-control-next-icon" aria-hidden="true"></span> */}
+          <img src={icon} style={{ rotate: "0deg" }} width={40} alt="btn" />
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
-
-    </div>
-
-    <div class="carousel-item d-flex justify-content-center">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrg85UaJB1LbsSBZW4mOGAB7k1mZUA21UwBg&usqp=CAU" class="d-block slider-img-size" alt="..."/>
-      
-      <div class="img-slider-text-container">
-        <div className="img-slider-text">
-          <div className="s-text">
-            <h1>AUTUMN SALE - UP TO 75% OFF!</h1>
-            <h5>Embark on the gaming season ahead.</h5>
-          </div>
-          <div className="s-button mt-2 mt-sm-3 mt-lg-5">
-            <button className="btn-cus btn-radius btn-blue-glow s-btn-cus me-md-5 mb-1 mb-sm-4 mb-md-0" >VISIT WEBSITE</button>
-            <button className="btn-cus btn-radius btn-blue-glow s-btn-cus" >BUY NOW</button>
-          </div>
-        </div>
-      </div>
-
-    </div>
-    
-
-  </div>
-  <button class="carousel-control-prev" style={{top:'unset',bottom:'80px'}} type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    {/* <span class="carousel-control-prev-icon" aria-hidden="true"></span> */}
-    <img src={icon} style={{rotate: "180deg"}} width={40} alt="btn" />
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" style={{top:'unset',bottom:'80px'}} type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    {/* <span class="carousel-control-next-icon" aria-hidden="true"></span> */}
-    <img src={icon} style={{rotate: "0deg"}} width={40} alt="btn" />
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 
     </>
   )
