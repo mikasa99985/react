cc0d40b779e2928db58ae446c9efcf4114c6ae0b
Akash <debnathakash911@gmail.com>
2023-10-08 11:54:22 +0530

    made game specifications

--- src/components/details/Specification.jsx
+++ src/components/details/Specification.jsx
@@ -4,6 +4,84 @@ const Specification = () => {
   return (
     <>
       <h4>Specification new</h4>
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col " >
+              <div className="row mb-4">
+              <div className="col mx-4">
+                <h5>Minimum Requirements</h5>
+                <div className="row">
+                  <div className="col-12">OS:<br />Windows 10/11</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">CPU:<br />INTEL@Core<sup>TM</sup> i7-4790K (INTEL@ Core-i5-8400 for Intel@ ARC<sup>TM</sup> <br />with Rebar) / Ryzen<sup>TM</sup> 5 1600</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">GPU:<br />Intel@ Arc <sup>TM</sup> A380 (6GB) / NVIDIA@ Geforce@ GTX 1060 (6 GB)/AMD Radeon <sup>TM</sup> RX 570 (4 GB)</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Memory:<br />8 GB (dual-channel mode)</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Storage:<br />40 GB</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">DirectX:<br />DX12</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Resolution:<br />1080p</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Preset/Target:<br />Low/30fps</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Peripherals:<br />Mouse, Keyboard, Controller</div>
+                </div>
+              </div>
+             </div>
+            </th>
+
+            <th scope="col"  >
+              <div className="row mb-5">
+              <div className="col mx-4">
+                <h5>Recommended</h5>
+                <div className="row">
+                  <div className="col-12">OS:<br />Windows 10/11</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">CPU:<br />INTEL@Core<sup>TM</sup> i7-8700K/AMD Ryzen<sup>TM</sup>5 3600</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">GPU:<br />Intel@ Arc <sup>TM</sup> A750 (8GB) / NVIDIA@ Geforce@ GTX 1660 Ti (6 GB) / AMD Radeon <sup>TM</sup> RX 5600 XT(6 GB)</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Memory:<br />16 GB (dual-channel mode)</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Storage:<br />40 GB</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">DirectX:<br />DX12</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Resolution:<br />1080p</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Preset/Target:<br />High/60fps</div>
+                </div>
+                <div className="row">
+                  <div className="col-12">Peripherals:<br />Mouse, Keyboard, Controller</div>
+                </div>
+              </div>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+
     </>
   )
 }
