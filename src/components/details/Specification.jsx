45f336ecbd42434a5c07ded325dbbef7669624d2
Akash <debnathakash911@gmail.com>
2023-10-10 13:50:18 +0530

    edited the text in specification

--- src/components/details/Specification.jsx
+++ src/components/details/Specification.jsx
@@ -4,7 +4,7 @@ const Specification = () => {
   return (
     <>
       <h4>Specification new</h4>
-      <table className="table table-dark">
+      <table className="table" >
         <thead>
           <tr>
             <th scope="col " >
@@ -12,31 +12,40 @@ const Specification = () => {
               <div className="col mx-4">
                 <h5>Minimum Requirements</h5>
                 <div className="row">
-                  <div className="col-12">OS:<br />Windows 10/11</div>
+                  <div className="col-12 ">
+                  <span style={{ color: 'red' }}>OS:</span><br />Windows 10/11</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">CPU:<br />INTEL@Core<sup>TM</sup> i7-4790K (INTEL@ Core-i5-8400 for Intel@ ARC<sup>TM</sup> <br />with Rebar) / Ryzen<sup>TM</sup> 5 1600</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>CPU:</span><br />INTEL@Core<sup>TM</sup> i7-4790K (INTEL@ Core-i5-8400 for Intel@ ARC<sup>TM</sup> <br />with Rebar) / Ryzen<sup>TM</sup> 5 1600</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">GPU:<br />Intel@ Arc <sup>TM</sup> A380 (6GB) / NVIDIA@ Geforce@ GTX 1060 (6 GB)/AMD Radeon <sup>TM</sup> RX 570 (4 GB)</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>GPU:</span><br />Intel@ Arc <sup>TM</sup> A380 (6GB) / NVIDIA@ Geforce@ GTX 1060 (6 GB)/AMD Radeon <sup>TM</sup> RX 570 (4 GB)</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Memory:<br />8 GB (dual-channel mode)</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}> Memory:</span><br />8 GB (dual-channel mode)</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Storage:<br />40 GB</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>Storage:</span><br />40 GB</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">DirectX:<br />DX12</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}> DirectX:</span><br />DX12</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Resolution:<br />1080p</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}> Resolution:</span><br />1080p</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Preset/Target:<br />Low/30fps</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>Preset/Target:</span><br />Low/30fps</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Peripherals:<br />Mouse, Keyboard, Controller</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>Peripherals:</span><br />Mouse, Keyboard, Controller</div>
                 </div>
               </div>
              </div>
@@ -47,31 +56,40 @@ const Specification = () => {
               <div className="col mx-4">
                 <h5>Recommended</h5>
                 <div className="row">
-                  <div className="col-12">OS:<br />Windows 10/11</div>
+                  <div className="col-12 "></div>
+                  <span style={{ color: 'red' }}>OS:</span> Windows 10/11
                 </div>
                 <div className="row">
-                  <div className="col-12">CPU:<br />INTEL@Core<sup>TM</sup> i7-8700K/AMD Ryzen<sup>TM</sup>5 3600</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>CPU:</span><br />INTEL@Core<sup>TM</sup> i7-8700K/AMD Ryzen<sup>TM</sup>5 3600</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">GPU:<br />Intel@ Arc <sup>TM</sup> A750 (8GB) / NVIDIA@ Geforce@ GTX 1660 Ti (6 GB) / AMD Radeon <sup>TM</sup> RX 5600 XT(6 GB)</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>GPU:</span><br />Intel@ Arc <sup>TM</sup> A750 (8GB) / NVIDIA@ Geforce@ GTX 1660 Ti (6 GB) / AMD Radeon <sup>TM</sup> RX 5600 XT(6 GB)</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Memory:<br />16 GB (dual-channel mode)</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>Memory:</span><br />16 GB (dual-channel mode)</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Storage:<br />40 GB</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>Storage:</span><br />40 GB</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">DirectX:<br />DX12</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>DirectX:</span><br />DX12</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Resolution:<br />1080p</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>Resolution:</span><br />1080p</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Preset/Target:<br />High/60fps</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>Preset/Target:</span><br />High/60fps</div>
                 </div>
                 <div className="row">
-                  <div className="col-12">Peripherals:<br />Mouse, Keyboard, Controller</div>
+                  <div className="col-12">
+                  <span style={{ color: 'red' }}>Peripherals:</span><br />Mouse, Keyboard, Controller</div>
                 </div>
               </div>
               </div>
@@ -81,6 +99,8 @@ const Specification = () => {
         <tbody>
         </tbody>
       </table>
+      
+    
 
     </>
   )
