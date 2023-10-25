a225690e6f37dce252014afd6795740fe9f3041d
Soubhik <soubhik960@gmail.com>
2023-10-25 22:35:44 +0530

    fix collection 1

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -23,62 +23,62 @@ const Collection = () => {
   const limit = 10;
   const field = 'time';
 
-  // useEffectOnce(async () => {
+  useEffectOnce(async () => {
 
-  //   let query_db = db.collection("game_collection").orderBy(field).limit(limit);
+    let query_db = db.collection("game_collection").orderBy(field).limit(limit);
 
-  //   if (query.get('search') != null) {
-  //     // console.log('search =', query.get('search'));
-  //     let str = capitalize(query.get('search'));
+    if (query.get('search') != null) {
+      // console.log('search =', query.get('search'));
+      let str = capitalize(query.get('search'));
 
-  //     query_db = db.collection("game_collection")
-  //         .orderBy('name')
-  //         .startAt("[a-zA-Z0-9]*")
-  //         .endAt(str + '\uf8ff');
+      query_db = db.collection("game_collection")
+          .orderBy('name')
+          .startAt("[a-zA-Z0-9]*")
+          .endAt(str + '\uf8ff');
 
-  //     // query_db = db.collection("game_collection")
-  //     //         .where('name', '>=', query.get('search'))
-  //     //         .where('name', '<=', query.get('search') + '\uf8ff');
+      // query_db = db.collection("game_collection")
+      //         .where('name', '>=', query.get('search'))
+      //         .where('name', '<=', query.get('search') + '\uf8ff');
 
-  //   }
+    }
 
-  //   query_db.get().then((querySnapshot) => {
-  //     // console.log(querySnapshot);
-  //     if (!querySnapshot.empty) {
-  //       setfastVisible(querySnapshot.docs[0]);
-  //       setlastVisible(querySnapshot.docs[querySnapshot.docs.length - 1]);
-  //     } else {
-  //       setLoading(false);
-  //     }
+    query_db.get().then((querySnapshot) => {
+      // console.log(querySnapshot);
+      if (!querySnapshot.empty) {
+        setfastVisible(querySnapshot.docs[0]);
+        setlastVisible(querySnapshot.docs[querySnapshot.docs.length - 1]);
+      } else {
+        setLoading(false);
+      }
 
-  //     querySnapshot.forEach(async (doc) => {
-  //       // let rate = await database.ref(`game_collection/${doc.id}/ratings`).once("value");
-  //       // rate.forEach(element => {
-  //       //     console.log(element.val().rate);
-  //       // });
-  //       let arr = [];
-  //       database.ref(`game_collection/${doc.id}/ratings`).once('value', (snapshot) => {
-  //         // setLoading(false);
+      querySnapshot.forEach(async (doc) => {
+        // let rate = await database.ref(`game_collection/${doc.id}/ratings`).once("value");
+        // rate.forEach(element => {
+        //     console.log(element.val().rate);
+        // });
+        let arr = [];
+        database.ref(`game_collection/${doc.id}/ratings`).once('value', (snapshot) => {
+          // setLoading(false);
 
-  //         snapshot.forEach(
-  //           function (Childsnapshot) {
-  //             arr.push(parseInt(Childsnapshot.val().rate));
-  //           });
-  //       }).then(() => {
-  //         // setLoadingface(false);
-  //         setList(oldArray => [...oldArray, { doc: doc, rate: arr }]);
-  //         setLoading(false);
-  //       });
+          snapshot.forEach(
+            function (Childsnapshot) {
+              arr.push(parseInt(Childsnapshot.val().rate));
+            });
+        }).then(() => {
+          // setLoadingface(false);
+          setList(oldArray => [...oldArray, { doc: doc, rate: arr }]);
+          setLoading(false);
+        });
 
-  //     });
-  //     // setLoading(false);
-  //   }).then(() => {
+      });
+      // setLoading(false);
+    }).then(() => {
 
-  //   }).catch((error) => {
-  //     console.log("Error getting documents: ", error);
-  //   });
+    }).catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
 
-  // }, [])
+  }, [])
 
   // function next() {
   //   setLoading(true);
@@ -199,7 +199,7 @@ const Collection = () => {
         </div>
       </Banner>
 
-      {/* <div className="d-flex flex-wrap justify-content-center mx-auto w-res my-3">
+      <div className="d-flex flex-wrap justify-content-center mx-auto w-res my-3">
         {
           !loading ?
             List.length != 0 ?
@@ -233,7 +233,7 @@ const Collection = () => {
         }
       </div>
 
-      <div className="d-flex justify-content-between my-5">
+      {/* <div className="d-flex justify-content-between my-5">
         <button onClick={previous} className='btn'>
           <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
